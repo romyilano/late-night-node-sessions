89209653fde0f9f3458dab6cf99010335ac2e8b4
Romy Ilano <romy@snowyla.com>
2015-07-31 20:32:07 -0700

    added an error handler

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+var http = require("http");
 
 // Problem: we need a simple way to look at a user's badge count
 // and javascript points
+var username = "romyilano"; 
+
+function printMessage(username, badgeCount, points) {
+   var message = username + " has " + badgeCount + " total badge(s) and " + points + " points";
+   console.log(message);
+}
 
 // Connect to api url http://teamtreehouse.com/chalkers.json
+var request = http.get("teamtreehouse.com/" + username + ".json", function(response) {
 // read the data from the response
+   console.log(response.statusCode);
 // parse the data
-// print the data
+// print the data   
+});
+
+request.on("error", function(error) {
+   console.log(error.message);
+});
