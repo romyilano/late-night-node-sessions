e0bb5c93f8671bc42f376a4dbb98bb16da4729e1
Romy Ilano <romy@snowyla.com>
2015-07-31 20:17:51 -0700

    basics

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// Problem: we need a simple way to look at a user's badge count
+// and javascript points
+
+// Connect to api url http://teamtreehouse.com/chalkers.json
+// read the data from the response
+// parse the data
+// print the data
