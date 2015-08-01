2e80a9bf20ddd8d0c00c9ea64496609d5798a482
Romy Ilano <romy@snowyla.com>
2015-07-31 22:18:02 -0700

    now the command line works

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // Problem: we need a simple way to look at a user's badge count
 // and javascript points
-
 var profile = require("./profile.js"); // can also write profile without js
 var oakland_service = require("./oakland_service.js");
 
 var username = "romyilano"; 
+// var users = ["chalkers", "joykesten2", "romyilano"];
 
-oakland_service.fetchServiceRequests();
-profile.get(username);
+// console.dir(process.argv);
+var users = process.argv.slice(2);
+users.forEach(profile.get);
+
+// oakland_service.fetchServiceRequests();
+// profile.get(username);
+// profile.get("joykesten2");
+
+// this is weird! since there are exactly 1 callback for forEach and get()
+// it just gets passed back
+// users.forEach(profile.get);
