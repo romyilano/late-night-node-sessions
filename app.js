60a67173458b2e90a4dc4301f78d501af887bfba
Romy Ilano <romy@snowyla.com>
2015-07-31 21:24:52 -0700

    the things

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Problem: we need a simple way to look at a user's badge count
 // and javascript points
-var profile = require("./profile.js");
+
+var profile = require("./profile.js"); // can also write profile without js
 
 var username = "romyilano"; 
 var oaklandPublicServiceRequestsJSON = "http://data.oaklandnet.com/resource/quth-gb8e.json";
